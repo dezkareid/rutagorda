b1d1df4c3ced1ec821b407e8e900742e21be1a88
Joel Humberto Gomez Paredes <dezkareid@joels-mbp.gdl.mex.ibm.com>
2019-12-14 02:10:12 -0600

    added group  for endpoint

--- server/main.go
+++ server/main.go
@@ -6,13 +6,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func registerEndpoint(router *gin.Engine, path string, handler gin.HandlerFunc) (endpoint *gin.RouterGroup) {
+	endpoint = router.Group(path)
+	{
+		endpoint.GET("/", handler)
+	}
+	return
+}
+
+func rootHandler(context *gin.Context) {
+	response := gin.H{
+		"api":     "ruta gorda",
+		"version": 1,
+	}
+	context.JSON(http.StatusOK, response)
+}
+
+func getStablishments(context *gin.Context) {
+	response := []string{"taquitos"}
+	context.JSON(http.StatusOK, response)
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		data := gin.H{
-			"message": "Hello World",
-		}
-		c.JSON(http.StatusOK, data)
-	})
-	r.Run()
+	router := gin.Default()
+	router.GET("/", rootHandler)
+	registerEndpoint(router, "establishments", getStablishments)
+	router.Run()
 }
